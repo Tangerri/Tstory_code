095ec134ea3e7b4d30e31dcc10e918dad514cfc7
gyuri-guri <124rbfl@naver.com>
2022-11-07 17:31:21 +0900

    입력값 받기 게시

---
+++ 입력값_받기.py
@@ -0,0 +1,16 @@
+
+x = input()
+print("입력받은 값:"+x)
+
+x=input("x의 값은 : ")
+print(type(x))
+
+x=int(input())
+print(type(x))
+
+
+x, y= map(int, input().split())
+print(x,y)
+
+x, y= map(int, input().split(','))
+print(x,y)
